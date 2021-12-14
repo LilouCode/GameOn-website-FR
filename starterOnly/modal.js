637b4ce5c452c41173e9d4a499162aa2c1a3e6db
Lilou Code <lisa.music@outlook.com>
2021-12-14 10:31:05 +0100

    Liaison des labels HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,7 +14,18 @@ const formData = document.querySelectorAll(".formData");
 //ajouts
 // creations de la constante closeModal
 const closeModal = document.querySelectorAll(".close"); 
-
+//Liaisons des labels
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
