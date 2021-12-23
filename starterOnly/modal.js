641ca4c9145b4c44e00ec84f8820d1d9871e2244
Lilou Code <lisa.music@outlook.com>
2021-12-23 10:35:09 +0100

    Fix integration CSS et changement HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -47,7 +47,7 @@ const removeModal = () => {
 };
 
 // close modal event
-closeModal.forEach((close) => close.addEventListener("click", removeModal));
+// closeModal.forEach((close) => close.addEventListener("click", removeModal));
 
 // ******************************* Les ecoutes **********************
 // Ecouter la modification du prénom
@@ -112,16 +112,16 @@ form.addEventListener("submit", function (e) {
 const validFirstName = function (firstName) {
   let msg;
   let valid = false;
- // creation de la reg exp pour la validation du prénom 
+  // creation de la reg exp pour la validation du prénom
   let firstNameRegExp = new RegExp("^[a-zA-Z-À-ÖØ-öø-ÿ]+$", "g");
   //******* Doit contenir au minimum 2 caractères
   if (firstName.value.length < 2) {
     msg = "Vous devez saisir un minimum de 2 caractères pour ce champs";
   }
-  // ******* Doit contenir uniquement des caractères valides 
+  // ******* Doit contenir uniquement des caractères valides
   else if (!firstNameRegExp.test(firstName.value)) {
     msg = "Veuillez renseigner ce champs uniquement avec des caractères autorisés";
-  } 
+  }
   //******* Prénom valide
   else {
     msg = "Ce champs est valide";
@@ -210,7 +210,7 @@ const validBirthdate = function (birthdate) {
   let birthdateTest = birthdateRegExp.test(birthdate.value);
   // récupération de la balise span pour le message d'erreur
   let birthdateError = document.getElementById("birthdate_error");
-
+  console.log(birthdate.value);
   if (birthdateTest) {
     birthdateError.innerHTML = "";
     birthdateError.classList.remove("text-error");
@@ -228,8 +228,8 @@ const validBirthdate = function (birthdate) {
 const validQuantity = function (quantity) {
   // récupération de la balise span pour le message d'erreur
   let quantityError = document.getElementById("quantity_error");
-
-  if (/[0-99]/.test(quantity.value) && quantity.value.trim != "") {
+  console.log(/^[0-9]?[0-9]$/.test(quantity.value));
+  if (/^[0-9]?[0-9]$/.test(quantity.value) && quantity.value.trim != "") {
     quantityError.innerHTML = "";
     quantityError.classList.remove("text-error");
     quantity.classList.remove("text-control--error");
@@ -248,30 +248,21 @@ const validLocations = function (locations) {
   // récupération de la balise span pour le message d'erreur
   let locationsError = document.getElementById("locations_error");
 
-  let validQuantityCity = function (quantity) {
-    if (quantity.value > 0) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
   if (
-    validQuantityCity &&
-    (location1.checked == true ||
-      location2.checked == true ||
-      location3.checked == true ||
-      location4.checked == true ||
-      location5.checked == true ||
-      location6.checked == true)
+    location1.checked == true ||
+    location2.checked == true ||
+    location3.checked == true ||
+    location4.checked == true ||
+    location5.checked == true ||
+    location6.checked == true
   ) {
     locationsError.innerHTML = "";
     locationsError.classList.remove("text-error");
     return true;
-  } else {
-    locationsError.innerHTML = "Veuillez remplir ce champs";
-    locationsError.classList.add("text-error");
-    return false;
+} else {
+  locationsError.innerHTML = "Vous devez choisir une option";
+  locationsError.classList.add("text-error");
+  return false;
   }
 };
 
@@ -290,82 +281,3 @@ const validCheckbox1 = function (checkbox1) {
     return false;
   }
 };
-// form.addEventListener("submit", function validate (e) {
-
-//   // annulation du comportement par défaut
-//   e.preventDefault();
-
-//   // validation du prénom
-//     const firstNameError = document.getElementById ("first-name_error");
-
-//     //si le prénom est inférieur à 2 caractères
-//     if (firstName.value.trim = "" || firstName.value.trim < 2) {
-//       firstNameError.innerHTML = "Vous devez saisir un minimum de 2 caractère pour ce champs";
-//       firstNameError.addc
-//     }
-
-//     // si le prénom contient des caractères non valides
-//     else if (!nameRegExp.test(firstName.value)) {
-//       firstNameError.innerHTML = "Vous devez saisir un nom valide";
-//       firstNameError.style.color = "#e54858";
-//       firstNameError.style.fontSize = "0.4em";
-//     }
-
-//     else{
-//       firstNameValidation = true;
-//     };
-
-//     //validation du nom
-//     const lastNameError = document.getElementById ("last-name_error")
-//     //si le nom contient des caractères non valides
-//     if (!nameRegExp.test(lastName.value)) {
-//       lastNameError.innerHTML = "Vous devez saisir un nom valide";
-//       lastNameError.style.color = "#e54858";
-//       lastNameError.style.fontSize = "0.4em";
-//       //si le nom est inférieur à 2 caractères
-//     } else if (firstName.value.length < 2) {
-//       lastNameError.innerHTML = "Vous devez saisir un minimum de 2 caractère pour ce champs";
-//       lastNameError.style.color = "#e54858";
-//       lastNameError.style.fontSize = "0.4em";
-//     } else {
-//       validationLastName = true;
-//     };
-
-//     //validation de l'e-mail
-//     const emailError = document.getElementById ("email_error");
-//     let regExpEmail = new RegExp ("^([a-zA-Z0-9_.-]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z0-9]{2,3})$", "g");
-
-//     if (!regExpEmail.test(email)) {
-//       emailError.innerHTML = "Veuillez renseigner un email valide";
-//     } else if (email.value.trim ="") {
-//       emailError.innerHTML ="Veuiller renseigner ce champs";
-//     } else {
-//       validationEmail = true;
-//     };
-
-//     //validation de la date de naissance
-//     const birthdateError = document.getElementById ("birthdate_error");
-//     let regExpDate = new RegExp ("^([0-9]{2})|([0-9]{2})|([0-9]{4})$", "g");
-
-//     if (birthdate.value === "" || regExpDate.test(birthdate)) {
-//       birthdateError.innerHTML= "Veuillez renseigner ce champs";
-//       birthdateError.style.color = "red";
-//     } else{
-//       validationBirthdate = true;
-//     };
-
-//     //validation du nombre de tournois
-//     let quantityError = document.getElementById ("quantity_error");
-//     if (quantity.value= ""){
-//       quantityError.innerHTML = "Veuiller renseigner ce champs";
-//     } else {
-//       validationQuantity = true;
-//     };
-
-//     // Envoi et validation du formulaire
-//     if ((validationLastName == true) && (validationFirstName == true) && (validationEmail== true)
-//       && (validationBirthdate== true) && (validationQuantity== true)) {
-//       $("#submit").unbind('click').click();
-//     }
-
-//   });
